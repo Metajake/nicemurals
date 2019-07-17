1ab9e278d2f476c731f8e96024fa3be269b80996
metajake <jaynoco@gmail.com>
2019-07-17 17:22:15 -0500

    Added Porfolio Project model.

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 
-from .models import Work
+from .models import Work, Project
 
-admin.site.register(Work) 
+class ProjectAdmin(admin.ModelAdmin):
+    prepopulated_fields = {
+        'slug' : ('project_name',),
+    }
+
+admin.site.register(Work)
+admin.site.register(Project, ProjectAdmin)
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from ckeditor.fields import RichTextField
+
 class Work(models.Model):
     title = models.CharField(max_length=300, blank=True)
     caption = models.CharField(max_length=50, blank=True)
@@ -9,3 +11,14 @@ class Work(models.Model):
 
     def __str__(self):
         return self.title
+
+class Project(models.Model):
+    project_name = models.CharField(max_length = 200)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = RichTextField(blank=True)
+    featured_image = models.ImageField(upload_to="uploads/portfolio/", blank=True)
+    is_enabled = models.BooleanField(default=True)
+    images = models.ManyToManyField(Work, blank=True)
+
+    def __str__(self):
+        return self.project_name
