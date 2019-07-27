0f722f7fd132922c485d135640adcc89777c464f
metajake <jaynoco@gmail.com>
2019-07-27 18:37:42 -0500

    Blog Homepage Entry list behavior; Determine Entry featuredd image ratio, and apply style class.

--- blog/static/blog/js/blog-base.js
+++ blog/static/blog/js/blog-base.js
@@ -1,17 +1,30 @@
+// Add Animation to Masthead Homepage Anchor
 $('#home-link').click(function(e){
   e.preventDefault();
+
+  homepageDelay = 1000
   destination = $(this).attr('href')
 
   $(this).addClass("move")
 
   setTimeout(function(){
     window.location.href = destination;
-  }, 1000);
+  }, homepageDelay);
 })
 
+// Add Animation to Masthead Logout Anchor
 $('.fa-skull').click(function(e){
-  console.log("glick")
   $(this).addClass("spook")
-  // setTimeout(function(){console.log("Remobing");$(this).removeClass('spook')}, 1500);
   this.addEventListener("animationend", function(){$(this).removeClass('spook')})
 })
+
+// Homepage Entry List: Apply "featured image" ratio class to container
+$('.entry').each(function(index, entry){
+  $(entry).find('.figure.image').addClass( getImageRatioClass(entry) )
+})
+
+function getImageRatioClass(imgContainer){
+  imageRatio = Math.round( $(imgContainer).find('img').width() ) / Math.round( $(imgContainer).find('img').height() )
+  if(imageRatio > 1){orientation = 'is-horiz'}else{orientation = 'is-vert'}
+  return orientation
+}
