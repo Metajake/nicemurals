06c23ec889ca2691b87d87d25174e14f5895d461
metajake <jaynoco@gmail.com>
2019-07-27 19:13:59 -0500

    Homepage Entry Featured Image behavior: set margin-left style to horizontal images.

--- blog/static/blog/js/blog-base.js
+++ blog/static/blog/js/blog-base.js
@@ -19,12 +19,22 @@ $('.fa-skull').click(function(e){
 })
 
 // Homepage Entry List: Apply "featured image" ratio class to container
+// and Margin-Left to horizontal images
 $('.entry').each(function(index, entry){
   $(entry).find('.figure.image').addClass( getImageRatioClass(entry) )
+  setMarginLeftToHorizontalImages(entry)
 })
 
 function getImageRatioClass(imgContainer){
   imageRatio = Math.round( $(imgContainer).find('img').width() ) / Math.round( $(imgContainer).find('img').height() )
   if(imageRatio > 1){orientation = 'is-horiz'}else{orientation = 'is-vert'}
   return orientation
+}
+
+function setMarginLeftToHorizontalImages(imgContainer){
+  containerWidth = $(imgContainer).find('.figure.image').width()
+  imageWidth = $(imgContainer).find('.figure.image img').width()
+  difference = Math.round( Math.abs(containerWidth - imageWidth) )
+  marginLeft = Math.round( difference / 2 )
+  $(imgContainer).find('.figure.image img').css('margin-left', -marginLeft+'px')
 }
