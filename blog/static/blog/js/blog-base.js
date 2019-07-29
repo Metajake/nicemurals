8a507ebba2b2bdab813a50f2e5f69a715fcaba6c
metajake <jaynoco@gmail.com>
2019-07-29 01:15:22 -0500

    Blog entry list replace behavioral featured image margin with styled version.

--- blog/static/blog/js/blog-base.js
+++ blog/static/blog/js/blog-base.js
@@ -17,18 +17,3 @@ $('.fa-skull').click(function(e){
   $(this).addClass("spook")
   this.addEventListener("animationend", function(){$(this).removeClass('spook')})
 })
-
-function setImageMarginLeft(imgContainer){
-  image = $(imgContainer).find('img')
-  containerWidth = Math.abs( $(imgContainer).width() )
-  imageWidth = Math.abs( image.width() )
-  difference = Math.round( imageWidth - containerWidth )
-  marginLeft = Math.round( difference / 2 )
-  image.css('margin-left', -marginLeft+'px')
-}
-
-document.addEventListener("DOMContentLoaded", function(){
-  $('.landscape').each(function(index, imgContainer){
-    setImageMarginLeft(imgContainer)
-  })
-})
