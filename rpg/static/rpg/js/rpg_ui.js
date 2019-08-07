e59c23d328e70493b150ff57eb28cc6287a42384
metajake <jaynoco@gmail.com>
2019-08-07 10:19:04 -0500

    RPG UI, update behavior loading.

--- rpg/static/rpg/js/rpg_ui.js
+++ rpg/static/rpg/js/rpg_ui.js
@@ -6,7 +6,7 @@ var docHeight = Math.max( body.scrollHeight, body.offsetHeight,
 
 var worldBrand = document.querySelector('.world .is-size-brand')
 
-document.addEventListener("DOMContentLoaded", function(){
+$(window).bind("load", function(){
   $('.world').height(docHeight)
   worldBrand.style.marginTop = window.innerHeight/2 - worldBrand.clientHeight/2 + "px"
 })
