c02e5c58e9bb24fed26e00808c0ab22531470c46
metajake <jaynoco@gmail.com>
2019-08-23 17:10:44 -0500

    Blog Nav About link added animation.

--- rpg/static/rpg/js/location.js
+++ rpg/static/rpg/js/location.js
@@ -4,9 +4,11 @@ function worldFadeEvent(){
   existingAncestors.each(function(){
     $(this).css('pointer-events', 'auto');
   })
+  document.dispatchEvent(homeLinkEvent);
 }
 
 document.addEventListener("DOMContentLoaded", function(){
+  document.querySelector("#home-link").style.left = parseInt(-window.innerWidth / 6)+'px';
   world.addEventListener('transitionend', worldFadeEvent)
   world.addEventListener('webkitTransitionEnd', worldFadeEvent)
 })
