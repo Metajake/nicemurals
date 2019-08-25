b3131411aa43a8782dcfe6beb39f9470c3b3e337
metajake <jaynoco@gmail.com>
2019-08-25 18:44:46 -0500

    RPG add Story model to admin.

--- rpg/admin.py
+++ rpg/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from rpg.models import Entity
+from rpg.models import Entity, Story
 admin.site.register(Entity)
+admin.site.register(Story)
 admin.site.site_header = "First Studio.co"
